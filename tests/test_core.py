e21d7ac1f6e4314d0ac3a426a11309d8e22e684d
Alex Andonian <alexandonian@gmail.com>
2019-12-23 09:06:22 -0500

    Update tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,14 +2,10 @@ import pytest
 import torch
 
 from pretorched.data import samplers
-# import sys
-# sys.path.append('../')
-# import config as cfg
+
 from .. import config as cfg
 from .. import data
-# from .. import core, data
-# import config as cfg
-# import core, data
+
 
 DATA_ROOT = cfg.DATA_ROOT
 MAX_ITERS = 10
@@ -20,9 +16,12 @@ BATCH_SIZE = 16
     ('DFDC', 'train', 16, 224, 'DeepfakeFrame', 'DeepfakeSet'),
     ('DFDC', 'val', 16, 224, 'DeepfakeFrame', 'DeepfakeSet'),
     ('DFDC', 'train', 16, 224, 'DeepfakeFaceFrame', 'DeepfakeFaceSet'),
+    ('DFDC', 'val', 16, 224, 'DeepfakeFaceFrame', 'DeepfakeFaceSet'),
+    ('DFDC', 'train', 16, 224, 'DeepfakeFaceCropFrame', 'DeepfakeFaceSet'),
+    ('DFDC', 'val', 16, 224, 'DeepfakeFaceCropFrame', 'DeepfakeFaceSet'),
 ])
 def test_get_dataset(name, split, num_frames, size, dataset_type, record_set_type):
-    metadata = cfg.get_metadata(name, dataset_type=dataset_type,
+    metadata = cfg.get_metadata(name, split=split, dataset_type=dataset_type,
                                 data_root=DATA_ROOT)
 
     root = metadata['root']
@@ -32,9 +31,7 @@ def test_get_dataset(name, split, num_frames, size, dataset_type, record_set_typ
     Dataset = getattr(data, dataset_type, 'ImageFolder')
     transform = data.get_transform(split=split, size=size)
     dataset = Dataset(root, record_set, sampler, transform=transform)
-    # dataset = core.get_dataset(name=name, root=root,
-    #    split=split, size=224,
-    #    dataset_type=dataset_type)
+    print(len(dataset))
     frames, label = dataset[0]
     assert label < cfg.num_classes_dict[name]
     assert frames.shape == torch.Size((3, num_frames, size, size))
