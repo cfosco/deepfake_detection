fe595dd82ea953af93706fd73db56b680b1b2ca2
Alex Andonian <alexandonian@gmail.com>
2019-12-23 09:05:56 -0500

    Update train scripts

--- scripts/train_baseline.sh
+++ scripts/train_baseline.sh
@@ -3,6 +3,9 @@
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 python main.py \
-    --ddp -w 1 -r 0 -j 16 \
+    --ddp -w 1 -r 0 -j 12 \
+    -b 32 \
+    --segment_count 32 \
     -a resnet3d50 --pretrained moments \
+    --optimizer SGD \
     --dataset_type DeepfakeFaceFrame --record_set_type DeepfakeFaceSet
