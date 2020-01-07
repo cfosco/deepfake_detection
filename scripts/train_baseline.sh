53ff5203aa8e12efc0a0031c7516c4c1797af04a
Alex Andonian <alexandonian@gmail.com>
2020-01-07 03:00:40 -0500

    Update baseline training script

--- scripts/train_baseline.sh
+++ scripts/train_baseline.sh
@@ -2,10 +2,4 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-python main.py \
-    --ddp -w 1 -r 0 -j 12 \
-    -b 32 \
-    --segment_count 32 \
-    -a resnet3d50 --pretrained moments \
-    --optimizer SGD \
-    --dataset_type DeepfakeFaceFrame --record_set_type DeepfakeFaceSet
+python main.py --ddp -b 32 --segment_count 32 --pretrained moments --optimizer SGD
