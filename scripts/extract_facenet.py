3616d5230e81597efb31705d9d8f67f60bb52e56
Alex Andonian <alexandonian@gmail.com>
2020-01-22 14:04:30 -0500

    Update extraction script

--- scripts/extract_facenet.py
+++ scripts/extract_facenet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import contextlib
 import json
 import os
 import sys
@@ -15,8 +16,9 @@ from PIL import Image
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from models import FaceModel
+import pretorched
 from data import VideoFolder
+from models import FaceModel
 from pretorched.runners.utils import AverageMeter, ProgressMeter
 from pretorched.utils import chunk
 
@@ -24,6 +26,7 @@ STEP = 2
 CHUNK_SIZE = 150
 NUM_WORKERS = 2
 OVERWRITE = False
+REMOVE_FRAMES = False
 
 try:
     PART = sys.argv[1]
@@ -89,62 +92,69 @@ def main():
 
     # switch to evaluate mode
     model.eval()
+    dataloader = iter(dataloader)
     with torch.no_grad():
         end = time.time()
-        for i, (filename, x) in tqdm(enumerate(dataloader), total=len(dataloader)):
-            print(f'Extracting faces from: {filename}')
-            bs, nc, d, h, w = x.shape
-            x = x.permute(0, 2, 1, 3, 4).contiguous()  # [bs, d, nc, h, w]
-            x = x.view(-1, *x.shape[2:])  # [bs * d, nc, h, w]
+        for i in tqdm(range(len(dataloader)), total=len(dataloader)):
+            with contextlib.suppress(Exception):
+                filename, x = next(dataloader)
+                print(f'Extracting faces from: {filename}')
+                bs, nc, d, h, w = x.shape
+                x = x.permute(0, 2, 1, 3, 4).contiguous()  # [bs, d, nc, h, w]
+                x = x.view(-1, *x.shape[2:])  # [bs * d, nc, h, w]
 
-            save_dir = os.path.join(FACE_DIR, filename[0])
-            os.makedirs(save_dir, exist_ok=True)
-            save_paths = [os.path.join(save_dir, '{:06d}.jpg'.format(idx)) for idx in range(d)]
-            faces_out = []
-            for xx, ss in zip(chunk(x, CHUNK_SIZE), chunk(save_paths, CHUNK_SIZE)):
-                xx = xx.to(device)
-                xx.mul_(255.)
-                out = model.model(xx, smooth=True)
-                if not out:
-                    continue
-                out = torch.stack(out).cpu()
-                faces_out.append(out)
-            min_face = min([f.shape[1] for f in faces_out])
-            faces_out = torch.cat([f[:, :min_face] for f in faces_out])
-            face_images = {i: [Image.fromarray(ff.permute(1, 2, 0).numpy().astype(np.uint8)) for ff in f]
-                           for i, f in enumerate(faces_out.permute(1, 0, 2, 3, 4))}
-            del x
-            torch.cuda.empty_cache()
-            metadata = {
-                'filename': os.path.basename(filename[0]),
-                'num_faces': len(face_images),
-                'num_frames': [len(f) for f in face_images.values()],
-                'dir_tmpl': fdir_tmpl,
-                'im_tmpl': tmpl,
-                'full_tmpl': os.path.join(fdir_tmpl, tmpl),
-                'face_names': [fdir_tmpl.format(k) for k in face_images],
-                'face_nums': list(face_images.keys()),
-                'margin': margin,
-                'size': size,
-                'fps': 30,
-            }
+                save_dir = os.path.join(FACE_DIR, filename[0])
+                os.makedirs(save_dir, exist_ok=True)
+                save_paths = [os.path.join(save_dir, '{:06d}.jpg'.format(idx)) for idx in range(d)]
+                faces_out = []
+                for xx, ss in zip(chunk(x, CHUNK_SIZE), chunk(save_paths, CHUNK_SIZE)):
+                    xx = xx.to(device)
+                    xx.mul_(255.)
+                    out = model.model(xx, smooth=True)
+                    if not out:
+                        continue
+                    out = torch.stack(out).cpu()
+                    faces_out.append(out)
+                min_face = min([f.shape[1] for f in faces_out])
+                faces_out = torch.cat([f[:, :min_face] for f in faces_out])
+                face_images = {i: [Image.fromarray(ff.permute(1, 2, 0).numpy().astype(np.uint8)) for ff in f]
+                               for i, f in enumerate(faces_out.permute(1, 0, 2, 3, 4))}
+                del x
+                torch.cuda.empty_cache()
+                metadata = {
+                    'filename': os.path.basename(filename[0]),
+                    'num_faces': len(face_images),
+                    'num_frames': [len(f) for f in face_images.values()],
+                    'dir_tmpl': fdir_tmpl,
+                    'im_tmpl': tmpl,
+                    'full_tmpl': os.path.join(fdir_tmpl, tmpl),
+                    'face_names': [fdir_tmpl.format(k) for k in face_images],
+                    'face_nums': list(face_images.keys()),
+                    'margin': margin,
+                    'size': size,
+                    'fps': 30,
+                }
 
-            with open(os.path.join(save_dir, metadata_fname), 'w') as f:
-                json.dump(metadata, f)
+                with open(os.path.join(save_dir, metadata_fname), 'w') as f:
+                    json.dump(metadata, f)
 
-            for face_num, faces in face_images.items():
-                num_images = len(faces)
-                out_filename = os.path.join(save_dir, fdir_tmpl.format(face_num), tmpl)
-                with ThreadPool(num_images) as pool:
-                    names = (out_filename.format(i) for i in range(1, num_images + 1))
-                    list(pool.imap(save_image, zip(faces, names)))
-                    pool.close()
-                    pool.join()
-            # measure elapsed time
-            batch_time.update(time.time() - end)
-            end = time.time()
+                for face_num, faces in face_images.items():
+                    num_images = len(faces)
+                    out_filename = os.path.join(save_dir, fdir_tmpl.format(face_num), tmpl)
+                    with ThreadPool(num_images) as pool:
+                        names = (out_filename.format(i) for i in range(1, num_images + 1))
+                        list(pool.imap(save_image, zip(faces, names)))
+                        pool.close()
+                        pool.join()
+                    video_path = os.path.join(save_dir, fdir_tmpl.format(face_num))
+                    pretorched.data.utils.frames_to_video(f'{video_path}/*.jpg', video_path + '.mp4')
+                    if REMOVE_FRAMES:
+                        os.system(f'rm -rf {video_path}')
+                # measure elapsed time
+                batch_time.update(time.time() - end)
+                end = time.time()
 
-            progress.display(i)
+                progress.display(i)
 
 
 if __name__ == '__main__':
