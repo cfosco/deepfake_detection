cfe55b15c806d20b3ea62d32fa8fa6700a010c6c
Alex Andonian <alexandonian@gmail.com>
2020-01-22 04:35:39 -0500

    Update transforms

--- data/transforms.py
+++ data/transforms.py
@@ -1,25 +1,26 @@
 import torchvision
 
-import torchvideo
+from pretorched.data import transforms
 
 
 def get_transform(name='DFDC', split='train', size=224, resolution=256,
                   mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225],
-                  normalize=True):
-    norm = torchvideo.transforms.NormalizeVideo(mean=mean, std=std)
+                  normalize=True, degrees=25):
+    norm = transforms.NormalizeVideo(mean=mean, std=std)
     cropping = {
         'train': torchvision.transforms.Compose([
-            torchvideo.transforms.RandomResizedCropVideo(size),
-            torchvideo.transforms.RandomHorizontalFlipVideo()]),
+            transforms.RandomResizedCropVideo(size),
+            transforms.RandomHorizontalFlipVideo(),
+            transforms.RandomRotationVideo(degrees)]),
         'val': torchvision.transforms.Compose([
-            torchvideo.transforms.ResizeVideo(resolution),
-            torchvideo.transforms.CenterCropVideo(size),
+            transforms.ResizeVideo(resolution),
+            transforms.CenterCropVideo(size),
         ])
     }.get(split, 'val')
     transform = torchvision.transforms.Compose([
         cropping,
-        torchvideo.transforms.CollectFrames(),
-        torchvideo.transforms.PILVideoToTensor(),
-        norm if normalize else torchvideo.transforms.IdentityTransform(),
+        transforms.CollectFrames(),
+        transforms.PILVideoToTensor(),
+        norm if normalize else transforms.IdentityTransform(),
     ])
     return transform
