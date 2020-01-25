e3533749aa9b05e5929c6fdd52f17494853f65d8
Alex Andonian <alexandonian@gmail.com>
2020-01-24 21:32:26 -0500

    Add custom VideoToTensor transform

--- data/transforms.py
+++ data/transforms.py
@@ -1,3 +1,5 @@
+from typing import Iterator
+
 import numpy as np
 import torch
 import torchvision
@@ -146,3 +148,49 @@ class ToTensor(object):
 
     def __repr__(self):
         return self.__class__.__name__ + '()'
+
+
+class VideoToTensor:
+    """Convert a list of PIL Images to a tensor :math:`(C, T, H, W)` or
+    :math:`(T, C, H, W)`.
+    """
+
+    def __init__(self, rescale: bool = True, ordering: str = "CTHW"):
+        """
+        Args:
+            rescale: Whether or not to rescale video from :math:`[0, 255]` to
+                :math:`[0, 1]`. If ``False`` the tensor will be in range
+                :math:`[0, 255]`.
+            ordering: What channel ordering to convert the tensor to. Either `'CTHW'`
+                or `'TCHW'`
+        """
+        self.rescale = rescale
+        self.ordering = ordering.upper()
+        self.to_tensor = ToTensor(rescale)
+        acceptable_ordering = ["CTHW", "TCHW"]
+        if self.ordering not in acceptable_ordering:
+            raise ValueError(
+                "Ordering must be one of {} but was {}".format(
+                    acceptable_ordering, self.ordering))
+
+    def __call__(self, frames):
+        """
+        PIL Images are in the format (H, W, C)
+        F.to_tensor converts (H, W, C) to (C, H, W)
+        Since we have a list of these tensors, when we stack them we get shape
+        (T, C, H, W)
+        """
+        if isinstance(frames, Iterator):
+            frames = list(frames)
+        tensor = torch.stack(list(map(self.to_tensor, frames)))
+        if self.ordering == "CTHW":
+            tensor = tensor.transpose(0, 1)
+        return tensor
+
+    def __repr__(self):
+        return (
+            self.__class__.__name__
+            + "(rescale={rescale!r}, ordering={ordering!r})".format(
+                rescale=self.rescale, ordering=self.ordering
+            )
+        )
