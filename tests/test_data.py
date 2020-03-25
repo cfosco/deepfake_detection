5a25f1ee26f0bfaa6e6ca2f38276d72f73aaf9f8
Alex Andonian <alexandonian@gmail.com>
2020-03-25 04:28:03 -0400

    Update tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -13,19 +13,44 @@ BATCH_SIZE = 16
 
 SKIP_FRAME = True
 SKIP_VIDEO = False
+SKIP_VIDEO_ZIP = False
 
 
 @pytest.mark.skipif(SKIP_VIDEO, reason='Data not present')
 @pytest.mark.parametrize('name, split, num_frames, size, dataset_type, record_set_type', [
     ('DFDC', 'train', 16, 224, 'DeepfakeVideo', 'DeepfakeSet'),
     ('DFDC', 'val', 16, 224, 'DeepfakeVideo', 'DeepfakeSet'),
+    ('DFDC', 'train', 16, 224, 'DeepfakeFaceVideo', 'DeepfakeFaceSet'),
     ('DFDC', 'val', 16, 224, 'DeepfakeFaceVideo', 'DeepfakeFaceSet'),
+])
+def test_get_dataset_video(name, split, num_frames, size, dataset_type, record_set_type):
+    metadata = cfg.get_metadata(name, split=split, dataset_type=dataset_type,
+                                data_root=DATA_ROOT)
+    print(metadata)
+    root = metadata['root']
+    metafile = metadata['metafile']
+    blacklist_file = metadata['blacklist_file']
+    record_set = getattr(data, record_set_type)(metafile, blacklist_file=blacklist_file)
+    sampler = samplers.TSNFrameSampler(num_frames)
+    Dataset = getattr(data, dataset_type, 'ImageFolder')
+    transform = data.get_transform(split=split, size=size)
+    dataset = Dataset(root, record_set, sampler, transform=transform)
+    for i, (frames, label) in enumerate(dataset):
+        if not (i < MAX_ITERS):
+            break
+        print(i, frames.shape, label)
+        assert label < cfg.num_classes_dict[name]
+        assert frames.shape == torch.Size((3, num_frames, size, size))
+
+
+@pytest.mark.skipif(SKIP_VIDEO_ZIP, reason='Data not present')
+@pytest.mark.parametrize('name, split, num_frames, size, dataset_type, record_set_type', [
     ('DFDC', 'train', 16, 224, 'DeepfakeZipVideo', 'DeepfakeSet'),
     ('DFDC', 'val', 16, 224, 'DeepfakeZipVideo', 'DeepfakeSet'),
     ('DFDC', 'train', 16, 224, 'DeepfakeZipFaceVideo', 'DeepfakeFaceSet'),
     ('DFDC', 'val', 16, 224, 'DeepfakeZipFaceVideo', 'DeepfakeFaceSet'),
 ])
-def test_get_dataset_video(name, split, num_frames, size, dataset_type, record_set_type):
+def test_get_dataset_video_zip(name, split, num_frames, size, dataset_type, record_set_type):
     metadata = cfg.get_metadata(name, split=split, dataset_type=dataset_type,
                                 data_root=DATA_ROOT)
     print(metadata)
