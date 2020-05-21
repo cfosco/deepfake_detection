9ad7066414477aca21764a749d9afab2ad5b1b80
Alex Andonian <alexandonian@gmail.com>
2020-05-21 03:52:32 -0400

    Update train script for MagNet

--- models/deep_motion_mag/train.py
+++ models/deep_motion_mag/train.py
@@ -19,34 +19,94 @@ from dataloader import ImageFromFolder
 from utils import AverageMeter
 
 parser = argparse.ArgumentParser(description='PyTorch Deep Video Magnification')
-parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
-                    help='number of data loading workers (default: 0)')
-parser.add_argument('--epochs', default=25, type=int, metavar='N',
-                    help='number of total epochs to run')
-parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
-                    help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch-size', default=32, type=int,
-                    metavar='N', help='mini-batch size (default: 4)')
-parser.add_argument('--lr', '--learning-rate', default=0.0001, type=float,
-                    metavar='LR', help='initial learning rate')
-parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                    help='momentum')
-parser.add_argument('--weight-decay', '--wd', default=0.0, type=float,
-                    metavar='W', help='weight decay (default: 0.0)')
-parser.add_argument('--num_data', default=100000, type=int,
-                    help='number of total data sample used for training (default: 100000)')
-parser.add_argument('--print-freq', '-p', default=100, type=int,
-                    metavar='N', help='print frequency (default: 100)')
-parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                    help='path to latest checkpoint (default: none)')
-parser.add_argument('--ckpt', default='ckpt', type=str, metavar='PATH',
-                    help='path to save checkpoint (default: ckpt)')
+parser.add_argument(
+    '-j',
+    '--workers',
+    default=4,
+    type=int,
+    metavar='N',
+    help='number of data loading workers (default: 0)',
+)
+parser.add_argument(
+    '--epochs', default=25, type=int, metavar='N', help='number of total epochs to run'
+)
+parser.add_argument(
+    '--start-epoch',
+    default=0,
+    type=int,
+    metavar='N',
+    help='manual epoch number (useful on restarts)',
+)
+parser.add_argument(
+    '-b',
+    '--batch-size',
+    default=32,
+    type=int,
+    metavar='N',
+    help='mini-batch size (default: 4)',
+)
+parser.add_argument(
+    '--lr',
+    '--learning-rate',
+    default=0.0001,
+    type=float,
+    metavar='LR',
+    help='initial learning rate',
+)
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum')
+parser.add_argument(
+    '--weight-decay',
+    '--wd',
+    default=0.0,
+    type=float,
+    metavar='W',
+    help='weight decay (default: 0.0)',
+)
+parser.add_argument(
+    '--num_data',
+    default=100000,
+    type=int,
+    help='number of total data sample used for training (default: 100000)',
+)
+parser.add_argument(
+    '--print-freq',
+    '-p',
+    default=100,
+    type=int,
+    metavar='N',
+    help='print frequency (default: 100)',
+)
+parser.add_argument(
+    '--resume',
+    default='',
+    type=str,
+    metavar='PATH',
+    help='path to latest checkpoint (default: none)',
+)
+parser.add_argument(
+    '--ckpt',
+    default='ckpt',
+    type=str,
+    metavar='PATH',
+    help='path to save checkpoint (default: ckpt)',
+)
 parser.add_argument('--gpu', default=0, type=str, help='cuda_visible_devices')
-parser.add_argument('--weight_reg1', default=1.0, type=float,
-                    help='weight texture regularization loss  (default: 1.0)')
-parser.add_argument('--weight_reg2', default=1.0, type=float,
-                    help='weight shpae regularization loss  (default: 1.0)')
+parser.add_argument(
+    '--weight_reg1',
+    default=1.0,
+    type=float,
+    help='weight texture regularization loss  (default: 1.0)',
+)
+parser.add_argument(
+    '--weight_reg2',
+    default=1.0,
+    type=float,
+    help='weight shpae regularization loss  (default: 1.0)',
+)
 parser.add_argument('--dataset_dir', type=str, default='MotionMagnification/train')
+parser.add_argument('--num_resblk_enc', type=int, default=3)
+parser.add_argument('--num_resblk_man', type=int, default=1)
+parser.add_argument('--num_resblk_dec', type=int, default=9)
 args = parser.parse_args()
 
 
@@ -62,7 +122,11 @@ def main():
     print(args)
 
     # create model
-    model = MagNet()
+    model = MagNet(
+        num_resblk_enc=args.num_resblk_enc,
+        num_resblk_man=args.num_resblk_man,
+        num_resblk_dec=args.num_resblk_dec,
+    )
     model = torch.nn.DataParallel(model).cuda()
     print(model)
 
@@ -78,8 +142,11 @@ def main():
             losses_reg1 = checkpoint['losses_reg1']
             losses_reg2 = checkpoint['losses_reg2']
             losses_reg3 = checkpoint['losses_reg3']
-            print("=> loaded checkpoint '{}' (epoch {})"
-                  .format(args.resume, checkpoint['epoch']))
+            print(
+                "=> loaded checkpoint '{}' (epoch {})".format(
+                    args.resume, checkpoint['epoch']
+                )
+            )
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
 
@@ -93,25 +160,32 @@ def main():
     cudnn.benchmark = True
 
     # dataloader
-    dataset_mag = ImageFromFolder(os.path.join(DATA_ROOT, args.dataset_dir),
-                                  num_data=args.num_data, preprocessing=True)
-    data_loader = data.DataLoader(dataset_mag,
-                                  batch_size=args.batch_size,
-                                  shuffle=True,
-                                  num_workers=args.workers,
-                                  pin_memory=True)
+    dataset_mag = ImageFromFolder(
+        os.path.join(DATA_ROOT, args.dataset_dir),
+        num_data=args.num_data,
+        preprocessing=True,
+    )
+    data_loader = data.DataLoader(
+        dataset_mag,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.workers,
+        pin_memory=True,
+    )
 
     # loss criterion
     criterion = nn.L1Loss(size_average=True).cuda()
 
     # optimizer
-    optimizer = torch.optim.Adam(model.parameters(), args.lr,
-                                 betas=(0.9, 0.999),
-                                 weight_decay=args.weight_decay)
+    optimizer = torch.optim.Adam(
+        model.parameters(), args.lr, betas=(0.9, 0.999), weight_decay=args.weight_decay
+    )
 
     # train model
     for epoch in range(args.start_epoch, args.epochs):
-        loss_recon, loss_reg1, loss_reg2, loss_reg3 = train(data_loader, model, criterion, optimizer, epoch, args)
+        loss_recon, loss_reg1, loss_reg2, loss_reg3 = train(
+            data_loader, model, criterion, optimizer, epoch, args
+        )
 
         # stack losses
         losses_recon.append(loss_recon)
@@ -129,7 +203,8 @@ def main():
         }
 
         # save checkpoints
-        fpath = os.path.join(ckpt_dir, 'ckpt_e%02d.pth.tar' % (epoch))
+        save_name = f'ckpt_{args.num_resblk_enc}_{args.num_resblk_man}_{args.num_resblk_dec}_{epoch}.pth.tar'
+        fpath = os.path.join(ckpt_dir, save_name)
         torch.save(dict_checkpoint, fpath)
 
 
@@ -151,7 +226,7 @@ def train(loader, model, criterion, optimizer, epoch, args):
         # compute output
         amp_factor = amp_factor.unsqueeze(1).unsqueeze(1).unsqueeze(1)
         y_hat, rep_a, rep_b, rep_c = model(xa, xb, xc, amp_factor)
-        #v_c, m_c = model.encoder(xc)
+        # v_c, m_c = model.encoder(xc)
         v_a, m_a = rep_a  # v: texture, m: shape
         v_b, m_b = rep_b
         v_c, m_c = rep_c
@@ -178,16 +253,25 @@ def train(loader, model, criterion, optimizer, epoch, args):
         end = time.time()
 
         if i % args.print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'LossR1 {loss_reg1.val:.4f} ({loss_reg1.avg:.4f})\t'
-                  'LossR2 {loss_reg2.val:.4f} ({loss_reg2.avg:.4f})\t'
-                  'LossR3 {loss_reg3.val:.4f} ({loss_reg3.avg:.4f})'.format(
-                      epoch, i, len(loader), batch_time=batch_time, data_time=data_time,
-                      loss=losses_recon, loss_reg1=losses_reg1,
-                      loss_reg2=losses_reg2, loss_reg3=losses_reg3))
+            print(
+                'Epoch: [{0}][{1}/{2}]\t'
+                'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                'LossR1 {loss_reg1.val:.4f} ({loss_reg1.avg:.4f})\t'
+                'LossR2 {loss_reg2.val:.4f} ({loss_reg2.avg:.4f})\t'
+                'LossR3 {loss_reg3.val:.4f} ({loss_reg3.avg:.4f})'.format(
+                    epoch,
+                    i,
+                    len(loader),
+                    batch_time=batch_time,
+                    data_time=data_time,
+                    loss=losses_recon,
+                    loss_reg1=losses_reg1,
+                    loss_reg2=losses_reg2,
+                    loss_reg3=losses_reg3,
+                )
+            )
 
     return losses_recon.avg, losses_reg1.avg, losses_reg2.avg, losses_reg3.avg
 
