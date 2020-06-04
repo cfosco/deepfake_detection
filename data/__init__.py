2bb3a74cb90d90df63411f821c3da1ad1b36dd41
Alex Andonian <alexandonian@gmail.com>
2020-06-04 02:20:09 -0400

    Fix missing imports in data/__init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -1,8 +1,9 @@
 from .datasets import (DeepfakeFaceCropFrame, DeepfakeFaceFrame,
-                       DeepfakeFaceRecord, DeepfakeFaceSet, DeepfakeFaceVideo,
-                       DeepfakeFrame, DeepfakeSet, DeepfakeVideo,
-                       DeepfakeZipFaceVideo, DeepfakeZipVideo, VideoFolder, VideoZipFile,
-                       video_collate_fn)
+                       DeepfakeFaceHeatvolVideo, DeepfakeFaceRecord,
+                       DeepfakeFaceSet, DeepfakeFaceVideo, DeepfakeFrame,
+                       DeepfakeSet, DeepfakeVideo, DeepfakeZipFaceVideo,
+                       DeepfakeZipVideo, HeatvolBatchSampler, VideoFolder,
+                       VideoZipFile, video_collate_fn)
 from .transforms import get_transform
 
 __all__ = [
@@ -13,11 +14,12 @@ __all__ = [
     'DeepfakeFaceCropFrame',
     'DeepfakeFaceFrame',
     'DeepfakeSet',
+    'DeepfakeFaceHeatvolVideo',
     'DeepfakeFaceSet',
     'DeepfakeFaceRecord',
     'get_transform',
-    'video_collate_fn'
-    'DeepfakeZipVideo',
+    'video_collate_fn' 'DeepfakeZipVideo',
     'DeepfakeZipFaceVideo',
     'VideoZipFile',
+    'HeatvolBatchSampler',
 ]
